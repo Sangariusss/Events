cad39af295e55518b87650f3ea2facf9c32af6f9
Sangariusss <c.boklakh.volodymyr@student.uzhnu.edu.ua>
2025-05-16 12:00:34 +0300

    Update versionName to 0.3.0 in build.gradle.kts

--- app/src/main/java/com/komiker/events/glide/CircleCropTransformation.kt
+++ app/src/main/java/com/komiker/events/glide/CircleCropTransformation.kt
@@ -17,18 +17,14 @@ class CircleCropTransformation : BitmapTransformation() {
     }
 
     override fun transform(pool: BitmapPool, toTransform: Bitmap, outWidth: Int, outHeight: Int): Bitmap {
-        // Calculate the size for the square bitmap and the offset
         val size = min(toTransform.width, toTransform.height)
         val x = (toTransform.width - size) / 2
         val y = (toTransform.height - size) / 2
 
-        // Create a square bitmap from the original bitmap
         val squared = Bitmap.createBitmap(toTransform, x, y, size, size)
 
-        // Use the BitmapPool to get a new bitmap with the same size and configuration
         val result = pool.get(size, size, Bitmap.Config.ARGB_8888)
 
-        // Draw the circle cropped bitmap onto the result bitmap
         val canvas = Canvas(result)
         val paint = Paint()
         val shader = BitmapShader(squared, TileMode.CLAMP, TileMode.CLAMP)
@@ -37,9 +33,6 @@ class CircleCropTransformation : BitmapTransformation() {
 
         val radius = size / 2f
         canvas.drawCircle(radius, radius, radius, paint)
-
-        // No need to recycle 'squared' here; it's handled by Glide
-        // 'result' is returned to Glide, and Glide handles its recycling
 
         return result
     }
